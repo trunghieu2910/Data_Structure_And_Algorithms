ac0a122f9f41d944e9bdd13b18b5a42e1de00df2
trunghieu2910 <trunghieu291005@gmail.com>
2026-04-19 21:41:23 +0700

    add content

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the length of Array : ");
+        int n = input.nextInt();
+        int[]  arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = input.nextInt();
+        }
 
     }
 }
